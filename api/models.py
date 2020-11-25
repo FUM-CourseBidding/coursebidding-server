8f1be84ecde9a2669329cff1280bd97bb35a6e57
Mohammad Karami <1380mkkarami@gmail.com>
2020-11-25 06:02:24 +0330

    Add User model and binding other models with foreign key.

--- api/models.py
+++ api/models.py
@@ -1,15 +1,22 @@
 from django.db import models
 
+class User(models.Model):
+    username = models.CharField(max_length=254)
+    password = models.CharField(max_length=254)
 
 class Student(models.Model):
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
+    user = models.OneToOneField(User,
+        on_delete=models.PROTECT,
+        blank = True,
+    )
     name = models.CharField(max_length=50)
     last_semester_grade = models.PositiveSmallIntegerField()
     budget = models.PositiveSmallIntegerField()
 
 class Bid(models.Model):
-    username = models.CharField(max_length=50)
+    user = models.OneToOneField(User,
+        on_delete=models.CASCADE,
+    )
     course_code = models.PositiveSmallIntegerField()
     semester = models.DateField()
     value = models.PositiveSmallIntegerField()
