95cc63e91396f31d3f4538c64943f50a5fff8844
Amir Zolfaghari <amirzlf@disroot.org>
2020-12-06 20:49:29 +0330

    still checking github hooks

--- coursebidding/views.py
+++ coursebidding/views.py
@@ -6,7 +6,7 @@ def index(request):
               <p>
                 If you seek api documentations,you can find them <a href="http://api.fumcbm.tk/docs">here.</a>
                 <br>
-                This url should return 404 in the future(or maybe redirect to <b>/docs</b>).
+                Maybe we should redirect this url to <b>/docs</b> ....
               </p>
             </body>
           </html>      
