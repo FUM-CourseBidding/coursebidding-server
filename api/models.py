b396728b38ee54deb116ee359f7225bea9c1d5e9
Amir Zolfaghari <amirzlf@disroot.org>
2020-12-29 16:15:52 +0330

    add __str__ to all models

--- api/models.py
+++ api/models.py
@@ -11,18 +11,23 @@ class Student(AbstractUser):
     class Meta:
         verbose_name = _('student')
         verbose_name_plural = _('students')
-
+        
     def __str__(self):
         return self.username
 
 class Professor(models.Model):
     name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+    
 
 
 class Course(models.Model):
     code = models.PositiveIntegerField(primary_key=True)
     subject = models.CharField(max_length=50)
     unit = models.PositiveSmallIntegerField()
+    def __str__(self):
+        return self.subject
 class AvailableCourse(models.Model):
     semester = models.CharField(max_length=50)
     #semester is stored like XY-Z where XY is the year and Z
@@ -31,8 +36,13 @@ class AvailableCourse(models.Model):
     course = models.ForeignKey(Course,on_delete=models.CASCADE)
     group_number = models.PositiveSmallIntegerField()
     capacity = models.PositiveSmallIntegerField()
+    def __str__(self):
+        return f'{self.course}-{self.professor}'
 class Bid(models.Model):
     course = models.ForeignKey(AvailableCourse,on_delete=models.CASCADE)
     student = models.ForeignKey(Student,on_delete=models.CASCADE)
     value = models.PositiveSmallIntegerField()
+    def __str__(self):
+        return f'{self.student}-{self.course}-{self.value}'
+    
 
