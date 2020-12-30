965bd09fe6f13f3fdbcee6a3a404f20a1bcd1296
Amir Zolfaghari <amirzlf@disroot.org>
2020-12-30 10:01:36 +0330

    CORS implementation

--- coursebidding/settings.py
+++ coursebidding/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'dj_rest_auth',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -51,7 +52,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8000',
+    'http://localhost:3000',
+    'http://localhost:1000',
+    'http://localhost:3',
+    )
 
 ROOT_URLCONF = 'coursebidding.urls'
 
